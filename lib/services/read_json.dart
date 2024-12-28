3d03b6508e280a35fb71bebd2a4c341015dc3553
Fiorella <fiorellaroma98@gmail.com>
2024-12-27 19:13:34 -0800

    Agregando comentarios a la funcion loadPlayerJson

--- lib/services/read_json.dart
+++ lib/services/read_json.dart
@@ -1,5 +1,5 @@
-import 'dart:convert';
-import 'package:flutter/services.dart' show rootBundle;
+import 'dart:convert'; //Libreria de dart que ofrece funciones para codificar o decodificar datos como JSON
+import 'package:flutter/services.dart' show rootBundle; //Importa librería de Flutter que permite acceder a recursos del proyecto, como archivos locales. Aquí se usa rootBundle para cargar archivos desde los assets (carpetas dentro del proyecto Flutter, como assets/).
 import 'package:game_wolf/domain/player.dart';  // Asegúrate de importar la clase Person
 String pathFile = "assets/players.json";
 
@@ -7,13 +7,16 @@ String pathFile = "assets/players.json";
 //este proceso puede tardar algun tiempo y sera completado en el futuro
 Future <List<Player>> loadPlayersJson() async {
   //Cargar el archivo JSON desde los assets
+  //El resultado es un string (el archivo json en formato de texto y se almacena en jsonString)
   String jsonString = await rootBundle.loadString(pathFile); //Usamos await para esperar que la operacion de lectura termine antes de continuar con la ejecucion del codigo
-
-  //Decodificar el JSON
+  //Decodificar el JSON convierte el texto del archivo JSON en una lista dinamica ya que no se sabe
+  //los elementos que contendra exacmatende el archivo json
+  //jsonResponse es una lista de objetos que proviene del json decodificado
   List<dynamic> jsonResponse = jsonDecode(jsonString);
-  print('JSON RESPONSE ${jsonResponse}');
 
   //Convertir cada objeto JSON en una instancia de Player
+  //.map toma cada elemento de la lista y lo transforma, utiliza la funcion fromJson y crea una instancia del Player a partir del objeto JSON
+  //toList() convierte un iterable en una lista concreta de objetos Player
   List<Player> players = jsonResponse.map((data) => Player.fromJson(data)).toList();
 
   return players;
