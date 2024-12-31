eea1b36744d0d46cb9390111b29e7c81a13968dc
Fiorella <fiorellaroma98@gmail.com>
2024-12-30 22:55:25 -0800

    Cambiando nombre archivo a users.json, cambiando el path de read_json

--- lib/services/read_json.dart
+++ lib/services/read_json.dart
@@ -1,11 +1,11 @@
 import 'dart:convert'; //Libreria de dart que ofrece funciones para codificar o decodificar datos como JSON
 import 'package:flutter/services.dart' show rootBundle; //Importa librería de Flutter que permite acceder a recursos del proyecto, como archivos locales. Aquí se usa rootBundle para cargar archivos desde los assets (carpetas dentro del proyecto Flutter, como assets/).
-import 'package:game_wolf/domain/player.dart';  // Asegúrate de importar la clase Person
-String pathFile = "assets/players.json";
+import 'package:game_wolf/domain/user.dart';  // Asegúrate de importar la clase Person
+String pathFile = "assets/users.json";
 
 //La funcion devuelve un future<List<Player>> (Una lista de objetos Player), 
 //este proceso puede tardar algun tiempo y sera completado en el futuro
-Future <List<Player>> loadPlayersJson() async {
+Future <List<User>> loadPlayersJson() async {
   //Cargar el archivo JSON desde los assets
   //El resultado es un string (el archivo json en formato de texto y se almacena en jsonString)
   String jsonString = await rootBundle.loadString(pathFile); //Usamos await para esperar que la operacion de lectura termine antes de continuar con la ejecucion del codigo
@@ -17,7 +17,7 @@ Future <List<Player>> loadPlayersJson() async {
   //Convertir cada objeto JSON en una instancia de Player
   //.map toma cada elemento de la lista y lo transforma, utiliza la funcion fromJson y crea una instancia del Player a partir del objeto JSON
   //toList() convierte un iterable en una lista concreta de objetos Player
-  List<Player> players = jsonResponse.map((data) => Player.fromJson(data)).toList();
+  List<User> players = jsonResponse.map((data) => User.fromJson(data)).toList();
 
   return players;
 }
